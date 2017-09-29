eb369ad91175d6311f2ade9211c5c0d10f5a9903
Han Liang Wee Eric <eric.aka.darth_vader@hotmail.com>
2017-09-29 11:33:30 +0800

    Remove Test classes

--- src/main/java/sg/edu/nus/comp/cs3219r/BankAcc2.java
+++
@@ -1,5 +0,0 @@
-package sg.edu.nus.comp.cs3219r;
-
-public class BankAcc2 extends BankAccount{
- private Test t;
-}
--- src/main/java/sg/edu/nus/comp/cs3219r/Test.java
+++
@@ -1,7 +0,0 @@
-package sg.edu.nus.comp.cs3219r;
-
-public class Test {
-
-  public String owner;
-
-}
